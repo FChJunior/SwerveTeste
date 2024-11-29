c8994e24c22c55ff8a976c5bad1b882e4d46d30b
ChagasJunior <jniors75@gmail.com>
2024-11-29 15:30:20 -0300

    Teste Final

---
+++ src/main/java/frc/robot/Subsystems/PlayersController/PlayerController.java
@@ -0,0 +1,5 @@
+package frc.robot.Subsystems.PlayersController;
+
+public class PlayerController {
+    
+}
--- src/main/java/frc/robot/Subsystems/SwerveDrive/SwerveModules.java
+++ src/main/java/frc/robot/Subsystems/SwerveDrive/SwerveModules.java
@@ -22,11 +22,21 @@ public class SwerveModules {
     private CANSparkMax angulationMotor3;
     private CANSparkMax angulationMotor4;
 
+    double speedTrMotor1 = 0;
+    double speedTrMotor2 = 0;
+    double speedTrMotor3 = 0;
+    double speedTrMotor4 = 0;
+
+    double posAnMotor1 = 0;
+    double posAnMotor2 = 0;
+    double posAnMotor3 = 0;
+    double posAnMotor4 = 0;
+
     private XboxController player;
 
     private Pigeon2 pigeon;
 
-    double ref_encoder = 360 / 21.40;
+    double ref_encoder = 16.84;
     double ref_PID_P = 0.3;
     double ref_Wrapping = 0.3;
 
@@ -39,19 +49,21 @@ public class SwerveModules {
 
     void Spark_ConfigPID_Traction(CANSparkMax motor) {
         motor.getPIDController().setP(1);
-        motor.getPIDController().setP(0);
-        motor.getPIDController().setP(0);
+        motor.getPIDController().setI(0);
+        motor.getPIDController().setD(0);
         motor.getPIDController().setOutputRange(-1, 1);
+        motor.setInverted(true);
     }
 
     void Spark_ConfigPID_Angulation(CANSparkMax motor, double wrapping) {
-        motor.getPIDController().setP(0.3); // Ajuste este valor conforme necessário
+        motor.getPIDController().setP(0.05); // Ajuste este valor conforme necessário
         motor.getPIDController().setI(0.0); // Normalmente 0 para começar
-        motor.getPIDController().setD(0.1); // Ajuste para controlar oscilações
+        motor.getPIDController().setD(0.0); // Ajuste para controlar oscilações
         motor.getPIDController().setOutputRange(-wrapping, wrapping);
         motor.getPIDController().setPositionPIDWrappingEnabled(true);
-        motor.getPIDController().setPositionPIDWrappingMaxInput(21.40); // Certifique-se do limite correto
+        motor.getPIDController().setPositionPIDWrappingMaxInput(ref_encoder); // Certifique-se do limite correto
         motor.getPIDController().setPositionPIDWrappingMinInput(0);
+        //motor.getEncoder().setPositionConversionFactor(ref_encoder);
     }
 
     void Spark_Config_P(CANSparkMax motor1, CANSparkMax motor2, CANSparkMax motor3, CANSparkMax motor4, double p) {
@@ -81,7 +93,6 @@ public class SwerveModules {
         angulationMotor4 = new CANSparkMax(8, MotorType.kBrushless);
         angulationMotor4.setInverted(true);
 
-        
         tractionMotor1.restoreFactoryDefaults();
         tractionMotor2.restoreFactoryDefaults();
         tractionMotor3.restoreFactoryDefaults();
@@ -129,40 +140,28 @@ public class SwerveModules {
         angulationMotor3.setIdleMode(IdleMode.kBrake);
         angulationMotor4.setIdleMode(IdleMode.kBrake);
 
-        angulationMotor1.getEncoder().setPositionConversionFactor(1.0); // Ajuste conforme necessário
-        angulationMotor2.getEncoder().setPositionConversionFactor(1.0);
-        angulationMotor3.getEncoder().setPositionConversionFactor(1.0);
-        angulationMotor4.getEncoder().setPositionConversionFactor(1.0);
-
         angulationMotor1.getEncoder().setPosition(0);
         angulationMotor2.getEncoder().setPosition(0);
         angulationMotor3.getEncoder().setPosition(0);
         angulationMotor4.getEncoder().setPosition(0);
 
+        pigeon.setYaw(0);
+
     }
 
     public void SwerveDriving() {
-        double speedTrMotor1 = 0;
-        double speedTrMotor2 = 0;
-        double speedTrMotor3 = 0;
-        double speedTrMotor4 = 0;
-
-        double posAnMotor1 = 0;
-        double posAnMotor2 = 0;
-        double posAnMotor3 = 0;
-        double posAnMotor4 = 0;
-
         double speed = player.getRawAxis(3) / 4;
 
-        double leftJoystick_DEG = Math.toDegrees(Math.atan2(player.getLeftX(), player.getLeftY()));
-        double rightJoystick_DEG = Math.toDegrees(Math.atan2(player.getRightX(), player.getRightY()));
+        double leftJoystick_DEG = Math.toDegrees(Math.atan2(player.getLeftX(), -player.getLeftY()));
+        double rightJoystick_DEG = Math.toDegrees(Math.atan2(player.getRightX(), -player.getRightY()));
 
         double yaw = -pigeon.getYaw().getValueAsDouble();
         double absYaw = yaw % 360;
 
-        absYaw = absYaw < 0 ? absYaw + 360 : absYaw;
+        if (absYaw < 0)
+            absYaw += 360;
 
-        if (Analog_Simulated(4, 5, 0.15) && Analog_Simulated(0, 1, 0.15)) {
+        if (Analog_Simulated(4, 5, 0.15) && !Analog_Simulated(0, 1, 0.15)) {
             if (!var1) {
                 var1 = true;
 
@@ -171,10 +170,10 @@ public class SwerveModules {
                 angulationMotor3.getPIDController().setP(ref_PID_P);
                 angulationMotor4.getPIDController().setP(ref_PID_P);
 
-                angulationMotor1.getPIDController().setReference(225 / ref_encoder, ControlType.kPosition);
+                angulationMotor1.getPIDController().setReference(45 / ref_encoder, ControlType.kPosition);
                 angulationMotor2.getPIDController().setReference(315 / ref_encoder, ControlType.kPosition);
                 angulationMotor3.getPIDController().setReference(115 / ref_encoder, ControlType.kPosition);
-                angulationMotor4.getPIDController().setReference(45 / ref_encoder, ControlType.kPosition);
+                angulationMotor4.getPIDController().setReference(225 / ref_encoder, ControlType.kPosition);
             }
 
             speed = player.getRawAxis(4) / 4;
@@ -194,102 +193,116 @@ public class SwerveModules {
                     Spark_Config_P(angulationMotor1, angulationMotor2, angulationMotor3, angulationMotor4, ref_PID_P);
                 }
 
-                posAnMotor1 = ((absYaw + leftJoystick_DEG)) / ref_encoder;
-                posAnMotor2 = ((absYaw + leftJoystick_DEG)) / ref_encoder;
-                posAnMotor3 = ((absYaw + leftJoystick_DEG)) / ref_encoder;
-                posAnMotor4 = ((absYaw + leftJoystick_DEG)) / ref_encoder;
+                posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
+                posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
+                posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
+                posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
 
                 if (player.getRightX() > 0.15 || player.getRightX() < -0.15) {
+
                     if ((rightJoystick_DEG >= 135 || rightJoystick_DEG < -135)
                             && ((leftJoystick_DEG >= 135 || leftJoystick_DEG < -135))) {
                         if (absYaw >= 315 || absYaw < 45) {
+                            System.err.println("POS1");
                             posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
-                            posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
+                            posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + rightJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 45 || absYaw < 135) {
+                            System.err.println("POS2");
                             posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 135 || absYaw < 225) {
+                            System.err.println("POS3");
                             posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 225 || absYaw < 315) {
+                            System.err.println("POS4");
                             posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + rightJoystick_DEG) / ref_encoder;
                         }
-                    }
-                    if ((rightJoystick_DEG >= -135 || rightJoystick_DEG < -45)
+                    } else if ((rightJoystick_DEG >= -135 || rightJoystick_DEG < -45)
                             && ((leftJoystick_DEG >= -135 || leftJoystick_DEG < -45))) {
                         if (absYaw >= 315 || absYaw < 45) {
+                            System.err.println("POS5");
                             posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 45 || absYaw < 135) {
+                            System.err.println("POS6");
                             posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 135 || absYaw < 225) {
-                            posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
+                            System.err.println("POS7");
+                            posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
-                            posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
+                            posAnMotor4 = (absYaw + rightJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 225 || absYaw < 315) {
+                            System.err.println("POS8");
                             posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + rightJoystick_DEG) / ref_encoder;
                         }
-                    }
-                    if ((rightJoystick_DEG >= 45 || rightJoystick_DEG < 135)
+                    } else if ((rightJoystick_DEG >= 45 || rightJoystick_DEG < 135)
                             && ((leftJoystick_DEG >= 45 || leftJoystick_DEG < 135))) {
                         if (absYaw >= 315 || absYaw < 45) {
+                            System.err.println("POS9");
                             posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + rightJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 45 || absYaw < 135) {
+                            System.err.println("POS10");
                             posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + rightJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 135 || absYaw < 225) {
+                            System.err.println("POS11");
                             posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 225 || absYaw < 315) {
+                            System.err.println("POS12");
                             posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
                         }
-                    }
-                    if ((rightJoystick_DEG >= -45 || rightJoystick_DEG < 45)
+                    } else if ((rightJoystick_DEG >= -45 || rightJoystick_DEG < 45)
                             && ((leftJoystick_DEG >= -45 || leftJoystick_DEG < 45))) {
                         if (absYaw >= 315 || absYaw < 45) {
+                            System.err.println("POS13");
                             posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + leftJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 45 || absYaw < 135) {
+                            System.err.println("POS14");
                             posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + rightJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 135 || absYaw < 225) {
+                            System.err.println("POS15");
                             posAnMotor1 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor4 = (absYaw + rightJoystick_DEG) / ref_encoder;
                         } else if (absYaw >= 225 || absYaw < 315) {
+                            System.err.println("POS16");
                             posAnMotor1 = (absYaw + rightJoystick_DEG) / ref_encoder;
                             posAnMotor2 = (absYaw + leftJoystick_DEG) / ref_encoder;
                             posAnMotor3 = (absYaw + rightJoystick_DEG) / ref_encoder;
@@ -322,6 +335,17 @@ public class SwerveModules {
                 angulationMotor4.getPIDController().setReference(0, ControlType.kPosition);
             }
 
+            if (!Analog_Simulated(0, 1, 0.15) && !Analog_Simulated(4, 5, 0.15) && player.getRawAxis(3) < 0) {
+                tractionMotor1.set(0);
+                tractionMotor2.set(0);
+                tractionMotor3.set(0);
+                tractionMotor4.set(0);
+            }
+            tractionMotor1.set(speedTrMotor1);
+            tractionMotor2.set(speedTrMotor2);
+            tractionMotor3.set(speedTrMotor3);
+            tractionMotor4.set(speedTrMotor4);
+
             SmartDashboard.putNumber("Pigeon YAW", yaw);
             SmartDashboard.putNumber("Pigeon YAW ABS", absYaw);
             SmartDashboard.putNumber("Joystick LEFT", leftJoystick_DEG);
@@ -333,10 +357,6 @@ public class SwerveModules {
             SmartDashboard.putNumber("Encoder Motor 4", angulationMotor4.getEncoder().getPosition());
             SmartDashboard.putNumber("Speed", speed);
 
-            tractionMotor1.set(speedTrMotor1);
-            tractionMotor2.set(speedTrMotor2);
-            tractionMotor3.set(speedTrMotor3);
-            tractionMotor4.set(speedTrMotor4);
         }
 
     }
